aa6207f65fd0e70159fd3d7bc90083f8b4370051
iamvany30 <mrvanvany@gmail.com>
2026-05-07 22:37:59 +1100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import path from "path";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: './', 
-    plugins:[react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+export default defineConfig({
+  base: "./", 
+  plugins: [react(), tailwindcss(), viteSingleFile()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "."),
     },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
+  },
 });
